d70909dec52e160c354e08e3c93d3278f3154a2c
Justin <jhugon@cern.fake>
2015-06-08 12:56:18 -0400

    solve.py: now can solve Euler-Lagrange, too

--- solve.py
+++ solve.py
@@ -9,10 +9,22 @@ import scipy.integrate
 class SolveMech(object):
 
   def __init__(self,lagrangian,coordinates,velocities,t=Symbol("t"),derivative_suffix="_dot"):
+    """
+    Takes a lagrangian, made from sympy variables. Coordinates and velocities
+    are lists of sympy variables used in the lagrangian. Make sure that coorinates[i]
+    matches velocities[i].
+    """
+
     self.symComputer = SymComputer(lagrangian,coordinates,velocities,t=t)
     self.derivative_suffix = derivative_suffix
     self.diffeq = None
     self.constValsList = None
+
+  def __str__(self):
+    result = ""
+    result += "****SolveMech**** "
+    result += str(self.symComputer)
+    return result
 
   def __call__(self,yarr,t):
     #print "call: ",yarr,t
@@ -24,11 +36,63 @@ class SolveMech(object):
     return result
 
   def solveEulerLegrange(self,times,initialVals,constantValueDict):
+    """
+    Requires a list of time values to solve for, a list of initial values for
+    the coordinates concatenated with one for the velocities, and a dictionary of
+    values to assign to the constant variables (where the keys are sympy
+    variables).
+
+    returns a list of lists for each time point, where for each time
+    point is a list of values for each coord and each velocity in the
+    same order as in the initial values.
+
+    Order of initial values and results:
+
+    [coord1,coord2,...,vel_coord1,vel_coord2,...]
+
+    Make sure initial values are in the same order as the coords and velocities
+    fed to the contructor!
+    """
     eoms = self.symComputer.getEulerLegrangeEOMs()
-    #print eoms
     eomDict = self.rearangeForDerivs(eoms)
+    print self.symComputer.tdm
     print eomDict
+
+    varList = []
+    for coord in self.symComputer.coordinates:
+      varList.append(coord)
+    for vel in self.symComputer.velocities:
+      varList.append(vel)
+
+    constList = sorted(constantValueDict.keys())
+    self.constValsList = [constantValueDict[x] for x in constList]
+    allVarList = [self.symComputer.t]+varList+constList
+
+    ## create functions to be called with list [t]+[coord1,coord2,...]+[vel1,vel2,...]+[const1,const2,...]
+    exprList = []
+    for var in varList:
+      var_dot = self.symComputer.tdm[var]
+      if eomDict.has_key(var_dot):
+        exprList.append(eomDict[var_dot])
+      else:
+        exprList.append(var_dot)
+    print "vars:", varList
+    print "diffexps:", exprList
+    print "allVarList: ",allVarList
+    self.diffeq = []
+    for expr in exprList:
+      self.diffeq.append(sympy.lambdify(allVarList,expr))
+
+    #print "about to call odeint!"
+    #print "with ", self, initialVals, times
+    result = scipy.integrate.odeint(self,initialVals,times)
+    #print result
+
     self.diffeq = None
+    self.constValsList = None
+
+    return result
+
 
   def solveHamiltonian(self,times,initialVals,constantValueDict):
     """
@@ -44,7 +108,11 @@ class SolveMech(object):
     Order of initial values and results:
 
     [coord1,coord2,...,momentum_coord1,momentum_coord2,...]
+
+    Make sure initial values are in the same order as the coords and velocities
+    fed to the contructor!
     """
+
     eoms = self.symComputer.getHamiltonianEOMs()
     eomDict = self.rearangeForDerivs(eoms)
 
@@ -112,30 +180,32 @@ if __name__ == "__main__":
   M = Symbol("M")
   g = Symbol("g")
   constValsDict = {
-    m:1.,
-    M:2.,
-    g:1.,
+    m:20.,
+    M:0.,
+    g:10.,
   }
 
   times = scipy.linspace(0,1,20)
-  initialVals = [0.,0.0]
+  initialValsL = [0.,10.0]
+  initialValsH = [0.,10.0*constValsDict[m]]
   
 
   L = m*x_dot**2/2 - m*g*x
 
   sm = SolveMech(L,[x],[x_dot])
-  #sm.solveEulerLegrange(constValsDict)
-  timeSeries = sm.solveHamiltonian(times,initialVals,constValsDict)
-
-  #print timeSeries
+  timeSeriesH = sm.solveHamiltonian(times,initialValsH,constValsDict)
+  timeSeriesL = sm.solveEulerLegrange(times,initialValsL,constValsDict)
+  print sm
 
   mpl.subplot(2, 1, 1)
-  mpl.plot(times,timeSeries[:,0], 'bo-')
+  mpl.plot(times,timeSeriesH[:,0], 'b-')
+  mpl.plot(times,timeSeriesL[:,0], 'ro')
   mpl.ylabel('Position')
   mpl
   mpl.subplot(2, 1, 2)
-  mpl.plot(times,timeSeries[:,1], 'bo-')
+  mpl.plot(times,timeSeriesH[:,1], 'b-')
+  mpl.plot(times,timeSeriesL[:,1], 'ro')
   mpl.xlabel('time (s)')
-  mpl.ylabel('Momentum')
+  mpl.ylabel('Velocity/Momentum')
   mpl
   mpl.show()
